98e4d4180e048867eea81e7f1af157f1e32509ab
Alexey Gribov <m0chk0z@gmail.com>
2022-08-27 22:24:18 +0300

    feat: Implement IEnumerable<T> and IReadOnlyCollection<T> for ArrayStack

--- Algorithms/DataStructures/ArrayStack.cs
+++ Algorithms/DataStructures/ArrayStack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,16 +8,28 @@ using Algorithms.Interfaces;
 
 namespace Algorithms.DataStructures
 {
-    public class ArrayStack<T> : IStack<T>
+    public class ArrayStack<T> : IStack<T>, IEnumerable<T>, IReadOnlyCollection<T>
     {
         private DynamicArray<T> stack;
         private int topIndex;
+        private int version = 0;
         private const int EMPTY_INDEX = -1;
+
+        public int Count => topIndex + 1;
 
         public ArrayStack()
         {
             stack = new DynamicArray<T>();
             topIndex = EMPTY_INDEX;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return stack[index];
+            }
         }
 
         public void Push(T item)
@@ -49,6 +62,87 @@ namespace Algorithms.DataStructures
         public bool IsEmpty()
         {
             return topIndex == EMPTY_INDEX;
+        }
+
+        /// <summary>
+        /// Checks if index is in bounds of the stack.
+        /// </summary>
+        /// <param name="index">Index of element in the dynamic array.</param>
+        /// <returns>true, if index is in bounds, false otherwise</returns>
+        private bool IsIndexValid(int index)
+        {
+            bool isInBounds = index >= 0 && index <= topIndex;
+            if (isInBounds)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Throws exception if given index is out of bounds.
+        /// </summary>
+        /// <param name="index">Index to validate.</param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        private void ValidateIndex(int index)
+        {
+            if (!IsIndexValid(index))
+            {
+                throw new IndexOutOfRangeException("Index is out of range");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            private ArrayStack<T> stack;
+            private int version;
+            private int index;
+            private T current;
+            public T Current => current;
+            object IEnumerator.Current => current;
+
+            public Enumerator(ArrayStack<T> stack)
+            {
+                this.stack = stack;
+                version = stack.version;
+                index = 0;
+                current = default(T);
+            }
+
+            public void Dispose()
+            { }
+
+            public bool MoveNext()
+            {
+                if (version != stack.version)
+                {
+                    throw new InvalidOperationException("Enumeration failed: collection changed");
+                }
+                if (index >= stack.topIndex) // last element
+                {
+                    current = default(T);
+                    return false;
+                }
+                current = stack[index];
+                index++;
+                return true;
+            }
+
+            public void Reset()
+            {
+                version = stack.version;
+                index = 0;
+            }
         }
     }
 }
